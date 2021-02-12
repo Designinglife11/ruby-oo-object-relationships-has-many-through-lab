6bde959872b2f8c205ede02f10f7dbf1376bc5ec
Andrew <Designinglife11@gmail.com>
2021-02-12 07:20:04 -0500

    commit tree 1041b9134caaffe9c5d84f1d725c9578f5a91cf1

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,29 @@
 
+class Patient
+    attr_accessor :name
+    
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def appointments
+        Appointment.all.select do |appointment|
+            appointment.patient == self
+        end
+    end
+
+    def new_appointment(doctor, date)
+        Appointment.new(doctor, self, date)
+    end
+
+    def doctors
+        appointments.map(&:doctor)
+    end
+end
